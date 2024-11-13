8e58b5435ba7844fb953c6c1b1a5b6effa5ec4c3
Jairo Freire <jairofreireortiz10@gmail.com>
2024-11-13 08:02:01 -0500

    Interfaces for users and shops have beed added

---
+++ src/app/interfaces/shops.interface.ts
@@ -0,0 +1,12 @@
+export interface Shops{
+    name: string,
+    owner: string,
+    state: string
+}
+
+export interface HeadersUsers {
+    Nombre: string,
+    Dueno: string,
+    Estado: string,
+    Acciones: string
+}
---
+++ src/app/interfaces/users.interface.ts
@@ -0,0 +1,12 @@
+export interface Users{
+    user: string,
+    email: string,
+    rol: string
+}
+
+export interface HeadersUsers {
+    users: string,
+    rol: string,
+    emails: string,
+    actions: string
+}
